eb2d218425f1ba3822feae6c5a6161b1d0cae6d2
Angie-Porro <angiemautone18@gmail.com>
2025-10-23 21:08:09 -0300

    conflictos en AppRoutes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,5 +1,6 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import DashboardLayout from "../components/Layout/DashboardLayout";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
 import Dashboard from "../pages/Dashboard/Dashboard";
 import Login from "../pages/Login/Login";
 import Orders from "../pages/Orders/Orders";
@@ -7,32 +8,28 @@ import Products from "../pages/Products/Products";
 import ProductDetail from "../pages/Products/ProductDetail";
 
 const AppRoutes = () => {
-  const isAuthenticated = localStorage.getItem("isAuthenticated") === "true";
-
   return (
-    <Routes>
-      <Route
-        path="/login"
-        element={isAuthenticated ? <Navigate to="/dashboard" /> : <Login />}
-      />
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
 
-      <Route
-        path="/dashboard"
-        element={
-          isAuthenticated ? <DashboardLayout /> : <Navigate to="/login" />
-        }
-      >
-        <Route path="/products/:id" element={<ProductDetail />} />
-
-        <Route index element={<Dashboard />} />
-        <Route path="orders" element={<Orders />} />
-        <Route path="products" element={<Products />} />
-      </Route>
-
-      <Route path="/" element={<Navigate to="/dashboard" />} />
-
-      <Route path="*" element={<Navigate to="/dashboard" />} />
-    </Routes>
+        <Route
+          path="/*"
+          element={
+            <PrivateRoute>
+              <Layout>
+                <Routes>
+                  <Route path="/" element={<Dashboard />} />
+                  <Route path="/products" element={<Products />} />
+                  <Route path="/orders" element={<Orders />} />
+                  <Route path="/admins" element={<Admins />} />
+                </Routes>
+              </Layout>
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </Router>
   );
 };
 
