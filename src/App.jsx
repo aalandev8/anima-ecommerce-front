72a4068f7a6cd1c48670ad08f7965ad6d0c15443
aalandev8 <aalan.dev8@gmail.com>
2025-10-30 11:48:25 -0300

    Fix: removed unnecesary comments

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,13 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Navigate,
 } from "react-router-dom";
-
-// Rutas públicas (tuyas)
 import Home from "./pages/home/Home";
 import StoreList from "./pages/StoreList";
 import StorePage from "./pages/StorePage";
 import ProductDetail from "./pages/ProductDetail";
 
-// // Rutas del dashboard (de tu compañera)
-// import PrivateRoute from './routes/PrivateRoute';
-// import Layout from './components/Layout/Layout';
-// import Dashboard from './pages/Dashboard/Dashboard';
-// import Products from './pages/Products/Products';
-// import Orders from './pages/Orders/Orders';
-// import Admins from './pages/Admins/Admins';
-// import Login from './pages/Login/Login';
+
 
 function App() {
   return (
@@ -28,26 +18,6 @@ function App() {
         <Route path="/stores/:category" element={<StoreList />} />
         <Route path="/store/:storeId" element={<StorePage />} />
         <Route path="/store/:storeId/product/:id" element={<ProductDetail />} />
-
-        {/* ===== RUTAS DEL DASHBOARD (Admin) ===== */}
-        {/* <Route path="/login" element={<Login />} />
-        
-        <Route
-          path="/dashboard/*"
-          element={
-            <PrivateRoute>
-              <Layout />
-            </PrivateRoute>
-          }
-        >
-          <Route index element={<Dashboard />} />
-          <Route path="products" element={<Products />} />
-          <Route path="orders" element={<Orders />} />
-          <Route path="admins" element={<Admins />} />
-        </Route> */}
-
-        {/* Redirección por defecto */}
-        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );
